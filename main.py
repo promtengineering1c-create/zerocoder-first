3b7108ea9af40ac2d5e576c929b6c59aeceb3114
promtengineering1c-create <promtengineering1c@gmail.com>
2026-03-22 13:37:46 +0500

    сооздан класс и его описание

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-#print("Hello, World!")
+import datetime as time
+
+tasks = {}
+
+class Task:
+    description: str
+    deadline: time
+    done: bool
+
+    def __init__(self, description, deadline, done):
+        self.description = description
+        self.deadline = deadline
+        self.done = done
+
+    def info(self):
+        print(f"Задача: {self.description}")
+        print(f"Срок выполнения: {self.deadline}")
